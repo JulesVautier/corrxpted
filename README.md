1f838317d0d724dfb9792b789373d16dd83b3ac1
Jules Vautier <jules.vautier@epitech.eu>
2020-11-20 17:31:32 +0100

    readme1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# corrxpted
+
+![https://corrxpted.neocities.org/][logo]
+
+[I'm an inline-style link](https://www.google.com)
+
+[logo]: ./favicon/61bbPhKX4rL.jpg "c r r x p k"
--- script.js
+++
@@ -1,8 +0,0 @@
-function randomWords() {
-    console.log('randomwords')
-    console.log(document.body.textContent)
-}
-
-$(document).ready(function () {
-    randomWords()
-})
