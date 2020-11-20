3dc6fdb2d531eb8a15d2695a34d3a2b92ec64bea
Jules Vautier <jules.vautier@epitech.eu>
2020-11-20 19:16:21 +0100

    can play music

--- music.js
+++ music.js
@@ -1,1 +1,12 @@
 var myAudio = document.getElementById("myAudio");
+var playing = false
+
+musicButton = document.getElementById("musicButton")
+function play() {
+    console.log(playing)
+    if (!playing)
+        myAudio.play()
+    else
+        myAudio.pause()
+    playing = !playing
+}
