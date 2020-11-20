3d4699fe222501ded782abf945a76ab0b8a35956
Jules Vautier <jules.vautier@epitech.eu>
2020-11-20 19:29:44 +0100

    cool button

--- music.js
+++ music.js
@@ -3,10 +3,23 @@ var playing = false
 
 musicButton = document.getElementById("musicButton")
 function play() {
-    console.log(playing)
     if (!playing)
         myAudio.play()
     else
         myAudio.pause()
     playing = !playing
 }
+
+var words = ["words",  "down", "into", "deep",  "relaxing", "every", "you", "read", "your", "mind", "blank", "losing", "focus", "into", "falling", "deep", "unnecessary"]
+
+function randomInt( min, max ) {
+    return Math.floor(Math.random() * ( max - min ) + min);
+}
+
+function changeButtonInfos() {
+    console.log("here")
+    word = words[randomInt(0, words.length - 1)].split('').join(' ');
+    musicButton.textContent = word
+}
+changeButtonInfos()
+window.setInterval(changeButtonInfos,10)
