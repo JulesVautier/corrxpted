c46999ab25908a64ea95cbc07109b93c358f25de
Jules Vautier <jules.vautier@epitech.eu>
2020-11-27 16:20:08 +0100

    Box raduis 300000

--- skybox/skybox.js
+++ skybox/skybox.js
@@ -5,7 +5,7 @@ var mouse = new THREE.Vector2();
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,45,30000);
+    camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight,45,300000);
     camera.position.set(-900,-200,-900);
     renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(window.innerWidth,window.innerHeight);
