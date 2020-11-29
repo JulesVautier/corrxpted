4be9ce35ebcb4e2f0071bf5db19c04aa38856cd8
Jules Vautier <jules.vautier@epitech.eu>
2020-11-29 01:38:05 +0100

    2 texts

--- synthetised/synthetised.js
+++ synthetised/synthetised.js
@@ -13,7 +13,6 @@ class Particle {
 
         this.x = x
         this.y = y
-        console.log(x, y)
 
         this.size = size
         this.color = color
@@ -63,6 +62,9 @@ var update = function () {
     requestAnimationFrame(update);
 }
 
+var ctx = undefined
+var canvas1 = undefined
+
 function createCanvas() {
     let canvasContainer = document.getElementById('canvas-container')
     canvas1 = document.createElement("CANVAS");
@@ -71,9 +73,12 @@ function createCanvas() {
 }
 
 function createLetters(text) {
-    ctx.color = "#ffffff"
+    console.log(canvas1)
+    console.log(ctx)
+    ctx = canvas1.getContext('2d')
+    ctx.fillStyle = "#ececec";
     ctx.font = "30px Ubuntu"
-    ctx.fillText(text, 0, 30)
+    ctx.fillText(text, 30, 30)
     // const data = ctx.getImageData(0, 0, canvas1.width, canvas1.height)
     const data = ctx.getImageData(0, 0, 1000, 1000)
     console.log(data.data, data.width, data.height)
@@ -86,7 +91,7 @@ function createLetters(text) {
             // console.log(pixel1, pixel2, pixel3, pixel4)
             if (pixel1 > 0 || pixel2 > 0 || pixel3 > 0 || pixel4 > 0) {
                 // console.log(pixel1, pixel2, pixel3, pixel4)
-                particles.push(new Particle(x / 4, y / 4, "#ff0000", 1))
+                particles.push(new Particle(x / 4 + 100, y / 4 + 100, "#ff0000", 1))
                 // let color = "rgba(pixel1, pixel2, pixel3, pixel4)"
                 // console.log(color)
             }
