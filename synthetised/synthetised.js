51a3b59c2fe3f56564af36d9b6d8e185328060f4
Jules Vautier <jules.vautier@epitech.eu>
2020-11-29 01:27:09 +0100

    A letter

--- synthetised/synthetised.js
+++ synthetised/synthetised.js
@@ -1,9 +1,9 @@
-function randomInt( min, max ) {
-    return Math.floor(Math.random() * ( max - min ) + min);
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-function randomFloat( min, max ) {
-    return Math.random() * ( max - min ) + min;
+function randomFloat(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 class Particle {
@@ -69,11 +69,39 @@ function createCanvas() {
     ctx = canvas1.getContext('2d')
 }
 
+function createLetters(text) {
+    ctx.color = "#ffffff"
+    ctx.font = "30px Ubuntu"
+    ctx.fillText(text, 0, 30)
+    // const data = ctx.getImageData(0, 0, canvas1.width, canvas1.height)
+    const data = ctx.getImageData(0, 0, 1000, 1000)
+    console.log(data.data, data.width, data.height)
+    for (let y = 0; y < data.height; y++) {
+        for (let x = 0; x < data.width; x += 4) {
+            let pixel1 = data.data[data.width * y + x]
+            let pixel2 = data.data[data.width * y + x + 1]
+            let pixel3 = data.data[data.width * y + x + 2]
+            let pixel4 = data.data[data.width * y + x + 3]
+            // console.log(pixel1, pixel2, pixel3, pixel4)
+            if (pixel1 > 0 || pixel2 > 0 || pixel3 > 0 || pixel4 > 0) {
+                console.log(pixel1, pixel2, pixel3, pixel4)
+                particles.push(new Particle(x, y, "#ff0000", 1))
+                // let color = "rgba(pixel1, pixel2, pixel3, pixel4)"
+                // console.log(color)
+            }
+            // console.log(pixel1)
+            //     if (pixel1 > 1)
+            //         console.log(pixel1)
+        }
+    }
+}
+
 function init() {
     createCanvas()
     setCanvasSize(canvas1)
     canvas1.onclick = createParticles;
     canvas1.ontouchstart = createParticles;
+    createLetters("A")
     update()
 }
 
