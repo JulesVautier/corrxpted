5acb97e2104839246fbe4ec5eadca64dcb788a46
Jules Vautier <jules.vautier@epitech.eu>
2020-12-01 23:24:02 +0100

    cool fonts

--- skybox/skybox.js
+++ skybox/skybox.js
@@ -6,6 +6,7 @@ var mouse = new THREE.Vector2();
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
 // Modal stuff
 
 function initModal() {
@@ -23,10 +24,10 @@ function initModal() {
         }
     }
 
-    document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementsByClassName("close")[0].onclick = function () {
         goalModal.style.display = "none";
     }
-    document.getElementsByClassName("close")[1].onclick = function() {
+    document.getElementsByClassName("close")[1].onclick = function () {
         helpModal.style.display = "none";
     }
 }
@@ -59,33 +60,33 @@ function createText(scene, text) {
     var texture1 = new THREE.Texture(canvas1)
     texture1.needsUpdate = true;
 
-    var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
+    var material1 = new THREE.MeshBasicMaterial({map: texture1, side: THREE.DoubleSide});
     material1.transparent = true;
 
     var mesh1 = new THREE.Mesh(
         new THREE.PlaneGeometry(canvas1.width, canvas1.height),
         material1
     );
-    mesh1.position.set(0,0,200);
-    scene.add( mesh1 );
+    mesh1.position.set(0, 0, 200);
+    scene.add(mesh1);
 
     setInterval(function () {
-        context1.clearRect(0,0,canvas1.width,canvas1.height)
+        context1.clearRect(0, 0, canvas1.width, canvas1.height)
         writeText("Try to think outside the box", context1)
         texture1.needsUpdate = true
     }, 100)
 }
 
 function writeText(initialText, ctx) {
-    let fonts = ["Times New Roman", "Ubuntu", "Arial", "Times", "Courier New", "Verdana", "Georgia", "Palantino", "Garamond"]
+    let fonts = ["Times New Roman", "Ubuntu", "Arial", "Times", "Courier New", "Verdana", "Georgia", "Palantino", "Garamond", "Ani", "aakar", "FreeMono", "DialogInput", "DejaVu Sans", "Doird Sans"]
     let font = fonts[randomInt(0, fonts.length)]
+    let px = randomInt(20, 40).toString() + "px"
     console.log(font)
-    ctx.clearRect(0,0,ctx.width,ctx.height)
-    ctx.font = "Bold 30px "+font;
+    ctx.clearRect(0, 0, ctx.width, ctx.height)
+    ctx.font = `Bold ${px} ${font}`
     ctx.fillStyle = "rgb(134,102,21)";
     ctx.fillText(initialText, 0, 40);
     ctx.needsUpdate = true
-
 }
 
 function init() {
